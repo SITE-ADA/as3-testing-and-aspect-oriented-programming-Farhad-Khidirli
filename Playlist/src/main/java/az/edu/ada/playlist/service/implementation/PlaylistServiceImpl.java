2464bed1223907cc09b499778fbe5a11604302cf
Farhad-Khidirli <farhad.xidirli@gmail.com>
2023-05-14 19:39:21 +0400

    Added Logging for given criterias to Service

--- Playlist/src/main/java/az/edu/ada/playlist/service/implementation/PlaylistServiceImpl.java
+++ Playlist/src/main/java/az/edu/ada/playlist/service/implementation/PlaylistServiceImpl.java
@@ -5,17 +5,19 @@ import az.edu.ada.playlist.entity.dto.PlaylistDTO;
 import az.edu.ada.playlist.mapper.PlaylistMapper;
 import az.edu.ada.playlist.repository.PlaylistRepository;
 import az.edu.ada.playlist.service.PlaylistService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class PlaylistServiceImpl implements PlaylistService {
 
     private final PlaylistRepository playlistRepository;
     private final PlaylistMapper playlistMapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlaylistServiceImpl.class);
 
     public PlaylistServiceImpl(PlaylistRepository playlistRepository, PlaylistMapper playlistMapper) {
         this.playlistRepository = playlistRepository;
@@ -24,7 +26,13 @@ public class PlaylistServiceImpl implements PlaylistService {
 
     @Override
     public Optional<PlaylistDTO> getById(Long id) {
+        LOGGER.info("Getting playlist by id {}", id);
+        long startTime = System.currentTimeMillis();
+
         Optional<Playlist> playlistOptional = playlistRepository.findById(id);
+
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("Got playlist by id {} in {}ms", id, endTime - startTime);
         if (playlistOptional.isPresent()) {
             PlaylistDTO playlistDTO = playlistMapper.toDto(playlistOptional.get());
             return Optional.of(playlistDTO);
@@ -35,31 +43,54 @@ public class PlaylistServiceImpl implements PlaylistService {
 
 
     public List<PlaylistDTO> list() {
+        LOGGER.info("Listing all playlists");
+        long startTime = System.currentTimeMillis();
+
         List<Playlist> playlists = playlistRepository.findAll();
+
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("Listed all playlists in {}ms", endTime - startTime);
         return playlistMapper.toPlaylistDTOList(playlists);
     }
 
     @Override
     public PlaylistDTO update(Long id, PlaylistDTO playlistDTO) {
+        LOGGER.info("Updating playlist with id {} with data {}", id, playlistDTO);
+        long startTime = System.currentTimeMillis();
         Playlist playlist = playlistMapper.toEntity(playlistDTO);
 
         playlist.setId(id);
 
+        long endTime = System.currentTimeMillis();
         Playlist updatedPlaylist = playlistRepository.save(playlist);
+        LOGGER.info("Updated playlist with id {} in {}ms. Updated playlist data: {}", id, endTime - startTime, updatedPlaylist);
         return playlistMapper.toDto(updatedPlaylist);
     }
 
 
     @Override
     public void delete(Long id) {
+        LOGGER.info("Deleting playlist with id {}", id);
+        long startTime = System.currentTimeMillis();
+
         playlistRepository.deleteById(id);
+
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("Deleted playlist with id {} in {}ms", id, endTime - startTime);
     }
 
     @Override
     public PlaylistDTO save(PlaylistDTO playlistDTO) {
+        LOGGER.info("Saving playlist with data {}", playlistDTO);
+        long startTime = System.currentTimeMillis();
+
         Playlist playlist = playlistMapper.toEntity(playlistDTO);
         Playlist savedPlaylist = playlistRepository.save(playlist);
-        return playlistMapper.toDto(savedPlaylist);
+
+        long endTime = System.currentTimeMillis();
+        PlaylistDTO savedPlaylistDTO = playlistMapper.toDto(savedPlaylist);
+        LOGGER.info("Saved playlist with id {} in {}ms. Saved playlist data: {}", savedPlaylistDTO.getId(), endTime - startTime, savedPlaylistDTO);
+        return savedPlaylistDTO;
     }
 
 }
